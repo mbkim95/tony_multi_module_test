4e5d026fd6facec31562f72c079f27e7287c6d1c
tony.mb <tony.mb@kakaocorp.com>
2026-03-06 11:02:16 +0900

    feat: github action로 배포 가능하도록 설정

--- lib/tony_multi_module_test.dart
+++
@@ -1,8 +0,0 @@
-
-import 'tony_multi_module_test_platform_interface.dart';
-
-class TonyMultiModuleTest {
-  Future<String?> getPlatformVersion() {
-    return TonyMultiModuleTestPlatform.instance.getPlatformVersion();
-  }
-}
